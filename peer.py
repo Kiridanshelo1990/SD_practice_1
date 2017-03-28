8acd786d6218ec91089c749bc266635b0d706d31
Kiridanshelo1990 <angelmayorga_s@hotmail.com>
2017-03-29 00:03:28 +0200

    funcion "pull" añadida

--- peer.py
+++ peer.py
@@ -3,34 +3,55 @@ from pyactor.exceptions import TimeoutError
 
 
 class Peer(object):
-    _tell = ['announce_2_tracker', 'init_start', 'stop_interval', 'test']
-    _ask = ['get_peers']
-    _ref = []
+	_tell = ['announce_2_tracker', 'init_start', 'stop_interval', 'test', 'get_peers']
+	_ask = ['push']
+	_ref = []
 
     # Inicializador de la/as instancia/as del Peer
-    def __init__(self):
-        self.tracker = None
-        self.torrent_hash = None
-        self.interval1 = None
+	def __init__(self):
+		self.tracker = None
+		self.torrent_hash = None
+		self.interval1 = None
+		self.interval2 = None
+		self.keysList = {}
+		self.chunk_id = None
+		self.chunk_data = {}
 
-    def init_start(self, tracker_proxy, torrent_hash):
-        self.tracker = tracker_proxy
-        self.torrent_hash = torrent_hash
-        self.interval1 = interval(self.host, 4, self.proxy, 'announce_2_tracker')
+	# Programamos los intervalos de announce y get_peers en 9 y 2 segundos respectivamente
+	def init_start(self, tracker_proxy, torrent_hash):
+		self.tracker = tracker_proxy
+		self.torrent_hash = torrent_hash
+		self.interval1 = interval(self.host, 9, self.proxy, 'announce_2_tracker')
+		self.interval2 = interval(self.host, 2, self.proxy, 'get_peers')
 
-    def announce_2_tracker(self):
-        if self.tracker and self.torrent_hash:
-            tracker.announce(self.torrent_hash, str(self.proxy))
+	# Si tenemos el tracker y el torrent_hash, hacemos el announce
+	def announce_2_tracker(self):
+		if self.tracker and self.torrent_hash:
+			tracker.announce(self.torrent_hash, str(self.proxy)[23:27])
 
-    def stop_interval(self):
-        print "stopping interval"
-        self.interval1.set()
-
-    def test(self):
-        print self.proxy
-
+	# Si tenemos el torrent_hash, hacemos el get_peers
 	def get_peers(self):
-		pass
+		if self.torrent_hash:
+			self.keysList = tracker.get_peers(self.torrent_hash)
+
+	# Stop de los intervalos del announce y el get_peers
+	def stop_interval(self):
+		print "stopping interval"
+		self.interval1.set()
+		self.interval2.set()
+
+	# Intentamos acceder al dato registrado en la posicion chunk_id, si lo tenemos lo devolvemos, si no, devolvemos None
+	def push(self, chunk_id):
+		self.chunk_id = chunk_id
+		try:
+			return self.chunk_data[self.chunk_id]
+		except LookupError:
+			return None
+
+	# Comprovamos que mandamos solo el host
+	def test(self):
+		print self.proxy
+		print str(self.proxy)[23:27]
 
 
 if __name__ == "__main__":
